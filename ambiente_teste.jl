ae3de9163dee73522d1436851a7eec71bb575f35
Martin-Lobe <martinblobe@gmail.com>
2022-12-01 13:56:25 -0300

    Arquivos iniciais

---
+++ ambiente_teste.jl
@@ -0,0 +1,10 @@
+#Ambiente para testa as funcoes criadas
+using Random
+include("funcoes.jl")
+n=5
+m=5
+
+Matrix_exemplo = rand(Int8,(n,m))
+#B = triangularizar(A)
+print(A)
+print(A[2])
---
+++ funcoes.jl
@@ -0,0 +1,14 @@
+#Esse arquivo tera funcoes importantes para
+function lu(A)
+m,n=size(A)
+
+
+end
+
+function triangularizar(A)
+    m,n=size(A)
+    for i in 1:m
+        A[i]
+
+
+end
